f66419280c8586d7775bc7bcddaac607fe3eea99
Igor Martayan <igor@martayan.org>
2024-08-29 16:57:53 +0200

    Implement Tool for KFC, KMC and FASTK

---
+++ tools.py
@@ -0,0 +1,43 @@
+from tool import Tool, select_param, basename
+
+
+def kfc_cmd(fasta, **params):
+    k = select_param("k", params, 31)
+    m = select_param("m", params, 21)
+    t = select_param("threshold", params, 2)
+    out = f"out/{basename(fasta)}"
+    return f"./KFC/kfc build -k {k} -m {m} -t {t} -i {fasta} -o {out} -f"
+
+
+KFC = Tool("KFC", kfc_cmd)
+
+
+def kmc_cmd(fasta, **params):
+    k = select_param("k", params, 31)
+    if k > 256:
+        return None
+    t = select_param("threads", params, 1)
+    ci = select_param("threshold", params, 2)
+    out = f"out/{basename(fasta)}"
+    return f"./KMC/kmc -fa -k{k} -t{t} -ci{ci} {fasta} {out} tmp"
+
+
+KMC = Tool("KMC", kmc_cmd)
+
+
+def fastk_cmd(fasta, **params):
+    k = select_param("k", params, 31)
+    T = select_param("threads", params, 1)
+    t = select_param("threshold", params, 2)
+    out = f"out/{basename(fasta)}"
+    return f"./FASTK/FastK -v -k{k} -t{t} -T{T} {fasta} -N{out}"
+
+
+FASTK = Tool("FASTK", fastk_cmd)
+
+
+TOOLS = [
+    KFC,
+    KMC,
+    FASTK,
+]

1ccae806951d012f04533befd420e5a72bc3484c
Igor Martayan <igor@martayan.org>
2024-08-29 12:15:44 +0200

    Skip APT dependencies

--- install_all.sh
+++ install_all.sh
@@ -3,11 +3,6 @@ set -euxo pipefail
 
 git submodule update --init --recursive
 
-# https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
-if [ -f /etc/debian_version ]; then
-    bash install_apt_dependencies.sh
-fi
-
 bash install_kfc.sh
 bash install_kmc.sh
 bash install_fastk.sh

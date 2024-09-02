795e986445447ba9aa047c2a8caa9baeb3a69de3
Igor Martayan <igor@martayan.org>
2024-09-02 16:32:41 +0200

    Filter tools in plots, add units in labels, plot time/mem for the dataset

--- plots.py
+++ plots.py
@@ -1,5 +1,5 @@
 from tools import TOOLS
-from main import MAIN_FILE
+from main import MAIN_FILE, DEFAULT_K
 import os
 import json
 import pandas as pd
@@ -30,19 +30,24 @@ LABEL = {t: t for t in MARKERS} | {
     "mem": "RAM usage during construction (in MB)",
 }
 
+TOOL_NAMES = [tool.name for tool in TOOLS]
 LOG_DIRS = set(tool.log_dir for tool in TOOLS)
 LOGS = []
 for log_dir in LOG_DIRS:
     for log_file in os.listdir(log_dir):
         with open(f"{log_dir}/{log_file}") as f:
-            LOGS.append(json.load(f))
+            log = json.load(f)
+            if log["tool"] in TOOL_NAMES:
+                LOGS.append(log)
 DATA = pd.json_normalize(LOGS)
 DATA["memory"] /= 1000
+DATA["filesize"] /= 1_000_000
+DATA = DATA.sort_values(by=["filesize", "k"])
 
 os.makedirs(PLOT_DIR, exist_ok=True)
 
 plt.figure()
-sns.lineplot(
+ax = sns.lineplot(
     data=DATA[DATA["filename"] == MAIN_FILE],
     x="k",
     y="time",
@@ -54,10 +59,11 @@ sns.lineplot(
     markersize=MARKER_SIZE,
     alpha=0.5,
 )
+ax.set(xlabel="$k$", ylabel="Time (in s)")
 plt.savefig(f"{PLOT_DIR}/plot_time_vs_k.png")
 
 plt.figure()
-sns.lineplot(
+ax = sns.lineplot(
     data=DATA[DATA["filename"] == MAIN_FILE],
     x="k",
     y="memory",
@@ -69,4 +75,27 @@ sns.lineplot(
     markersize=MARKER_SIZE,
     alpha=0.5,
 )
+ax.set(xlabel="$k$", ylabel="Memory usage (in MB)")
 plt.savefig(f"{PLOT_DIR}/plot_mem_vs_k.png")
+
+plt.figure()
+ax = sns.barplot(
+    data=DATA[DATA["k"] == DEFAULT_K],
+    x="filesize",
+    y="time",
+    hue="tool",
+    palette=PALETTE,
+)
+ax.set(xlabel="Input size (in Mb)", ylabel="Time (in s)")
+plt.savefig(f"{PLOT_DIR}/plot_time_dataset.png")
+
+plt.figure()
+ax = sns.barplot(
+    data=DATA[DATA["k"] == DEFAULT_K],
+    x="filesize",
+    y="memory",
+    hue="tool",
+    palette=PALETTE,
+)
+ax.set(xlabel="Input size (in Mb)", ylabel="Memory usage (in MB)")
+plt.savefig(f"{PLOT_DIR}/plot_mem_dataset.png")

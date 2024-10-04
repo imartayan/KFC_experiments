a0189ed2bb2f9e211810d7d4246c7f1a1dcf112d
Igor Martayan <igor@martayan.org>
2024-10-04 11:46:45 +0200

    Compile KMC with MAX_K=1024

--- install_kmc.sh
+++ install_kmc.sh
@@ -2,6 +2,7 @@
 set -euo pipefail
 
 version="3.2.4"
+MAX_K=1024
 
 # https://stackoverflow.com/questions/394230/how-to-detect-the-os-from-a-bash-script
 platform="unknown"
@@ -26,11 +27,19 @@ else
     exit 1
 fi
 
-set -x
-
-mkdir -p KMC
-cd KMC
-wget -O KMC.tar.gz https://github.com/refresh-bio/KMC/releases/download/v${version}/KMC${version}.${platform}.${arch}.tar.gz
-tar -xf KMC.tar.gz --strip-components 1
-
-ulimit -n 2048
+if [ ! -d "KMC" ]; then
+    mkdir -p KMC
+    cd KMC
+    if [[ "$platform" == "linux" && "$arch" == "x64" ]]; then
+        echo "Building KMC from source with MAX_K=${MAX_K} (this may take a while)"
+        set -x
+        git clone --recurse-submodules https://github.com/refresh-bio/kmc.git src
+        cd src
+        sed -i -e "s/256/${MAX_K}/g" kmc_core/defs.h
+        make -j kmc kmc_dump kmc_tools
+        cp bin/* ..
+    else
+        wget -O KMC.tar.gz https://github.com/refresh-bio/KMC/releases/download/v${version}/KMC${version}.${platform}.${arch}.tar.gz
+        tar -xf KMC.tar.gz --strip-components 1
+    fi
+fi

740cda16d8e84bff7ffa6f9b563db3ab2f6c8aee
Igor Martayan <igor@martayan.org>
2024-10-04 11:47:05 +0200

    Detect jellyfish existing directory

--- install_jellyfish.sh
+++ install_jellyfish.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 set -euxo pipefail
 
-mkdir -p jellyfish
-cd jellyfish
-wget -O jellyfish.tar.gz https://github.com/gmarcais/Jellyfish/releases/download/v2.3.1/jellyfish-2.3.1.tar.gz
-tar -xf jellyfish.tar.gz --strip-components 1
-./configure
-make -j
+if [ ! -d "jellyfish" ]; then
+    mkdir -p jellyfish
+    cd jellyfish
+    wget -O jellyfish.tar.gz https://github.com/gmarcais/Jellyfish/releases/download/v2.3.1/jellyfish-2.3.1.tar.gz
+    tar -xf jellyfish.tar.gz --strip-components 1
+    ./configure
+    make -j
+fi
